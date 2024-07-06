9228a409b8032b377e78186c14b1475e0cc38dd9
ikh05 <mi3061923002@gmail.com>
2024-07-07 05:28:39 +0800

    a

--- public/assets/js/root.js
+++ public/assets/js/root.js
@@ -1,11 +1,11 @@
 // import {ajax} from './modules/functions.js';
 
-document.addEventListener('click', ()=>{
-	let el = document.documentElement;
-	if(el.requestFullscreen) el.requestFullscreen();
-	else if(el.webkitRequestFullscreen) el.webkitRequestFullscreen();
-	else if(el.msRequestFullscreen) el.msRequestFullscreen();
-});
+// document.addEventListener('click', ()=>{
+// 	let el = document.documentElement;
+// 	if(el.requestFullscreen) el.requestFullscreen();
+// 	else if(el.webkitRequestFullscreen) el.webkitRequestFullscreen();
+// 	else if(el.msRequestFullscreen) el.msRequestFullscreen();
+// });
 let message = document.querySelector('.alert');
 if(message !== null) {
 	setTimeout(()=>{
