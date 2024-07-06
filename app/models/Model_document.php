84246fdabea7e81ca9a7c66c5c6180c102e20101
ikh05 <mi3061923002@gmail.com>
2024-07-07 06:42:14 +0800

    h

--- app/models/Model_document.php
+++ app/models/Model_document.php
@@ -8,10 +8,10 @@ class Model_document{
 		foreach ($files as $key => $file) {
 			$name = $file['name'];
 			$tamp = $file['tmp_name'];
-			var_dump($file);
-			echo "<br>";
 			$eks = explode('.', $name);
 			$eks = end($eks);
+			var_dump($eks);
+			echo "<br>";
 			if(strtolower($eks) === 'pdf'){
 				$n = $key.'_'.$token.'_'.time().'.'.$eks;
 				if(move_uploaded_file($tamp, 'assets/document/'.$n)){
